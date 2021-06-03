9b2048d034b26e91f753e5a421b9866ebcdf3c5a
stephen-5846 <stephenmail001@gmail.com>
2021-06-03 11:06:50 +0530

    API details added

--- twitterClient/APIUtil/TwitterAPIUtil.py
+++ twitterClient/APIUtil/TwitterAPIUtil.py
@@ -8,6 +8,18 @@ class TwitterAPIUtil:
 
     @staticmethod
     def postStatus(tweetJson):
+        """curl - XPOST
+        --url
+        'https://api.twitter.com/1.1/statuses/update.json?status=hello'
+        --header
+        'authorization: OAuth
+        oauth_consumer_key = "oauth_customer_key",
+        oauth_nonce = "generated_oauth_nonce",
+        oauth_signature = "generated_oauth_signature",
+        oauth_signature_method = "HMAC-SHA1",
+        oauth_timestamp = "generated_timestamp",
+        oauth_token = "oauth_token",
+        oauth_version = "1.0"' """
         return True
 
     @staticmethod
@@ -15,6 +27,7 @@ class TwitterAPIUtil:
  #       response = "sample"
   #      print(configurations)
    #     return response
+        #https://api.twitter.com/2/users/2244994945/tweets?tweet.fields=created_at&max_results=100&start_time=2019-01-01T17:00:00Z&end_time=2020-12-12T01:00:00Z
         json_data = os.path.join(BASE_DIR, 'static', "SampleResponse.json")
         fileData = open(json_data, 'r')
         response = json.load(fileData)
